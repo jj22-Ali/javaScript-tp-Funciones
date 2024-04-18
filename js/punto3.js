b8dd57f859be920a18a32de53d2c683f757ab1d6
Juan Jose Figueroa <juanjosealifigueroa56@gmail.com>
2024-04-18 15:08:07 -0300

    punto 1 al 5 realizados

---
+++ js/punto3.js
@@ -0,0 +1,11 @@
+function promedio3(z,x,c){
+
+    let Prom = (z+x+c)/3;
+
+    return Prom;
+
+}
+
+var a=10, s=22, d=12;
+
+console.log(`El promedio de los valor ${a},${s},${d} es de: ${promedio3(a,s,d)}`);
---
+++ js/punto5.js
@@ -0,0 +1,6 @@
+function doble(x){
+    return 2 * x;
+}
+
+var a = parseInt(prompt("Ingrese cualquier valor: "));
+console.log(`El doble de ${a} es el valor ${doble(a)}`);
